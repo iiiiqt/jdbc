37bcada8cd36bb0c2402c22d9804bbf88cf3f624
iiiiqt <70530071+iiiiqt@users.noreply.github.com>
2022-02-20 13:54:53 +0800

    Update jd_rush_jinggengjcq_dapainew.js

--- jd_rush_jinggengjcq_dapainew.js
+++ jd_rush_jinggengjcq_dapainew.js
@@ -1,6 +1,7 @@
 /*
 https://jinggengjcq-isv.isvjcloud.com/fronth5/#/pages/unitedCardNew20220123/unitedCardNew20220123?actId=b2d6a99babf64595aac5_220123
 10-29 11-3
+111
 */
 const $ = new Env("通用大牌联合");
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
